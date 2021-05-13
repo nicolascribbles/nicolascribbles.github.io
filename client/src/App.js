bd0f92f3512ce6bd2294d36aa8aad50e08b2f258
Nicola Reyes <nreyes@intrinio.com>
2021-05-13 00:18:00 -0600

    client asset setup

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,40 @@ import {
     BrowserRouter,
     Switch,
     Route,
-    Link
+    Link,
+    NavLink
   } from 'react-router-dom';
 
 import ProtectedRoute from "./context/protectedRoute";
 import UserProvider, { UserContext } from "./context/User";
 import useDarkMode from "./context/darkMode";
 
-import Home from './components/home.component';
+import UserSVG from "./svgs/user";
+import UserCheckSVG from "./svgs/userCheck";
+import StarSVG from "./svgs/star";
+import CodeSVG from "./svgs/code";
+import DashboardSVG from "./svgs/dashboard";
+import CommentSVG from "./svgs/comment";
+import BookmarkSVG from "./svgs/bookmark";
+import StoreSVG from "./svgs/store";
+
+import Toggle from "./layout/toggle";
+import MobileMenuButton from "./layout/mobileMenuButton";
+import MobileMenu from "./layout/mobileMenu";
+import LightLogo from "./layout/lightLogo";
+import DarkLogo from "./layout/darkLogo";
+import California from "./layout/california";
+import GithubPicture from "./layout/githubPicture";
+
 import Login from './components/login.component';
 import RegisterUsers from './components/registerUsers.component';
-import Blogs from './components/blogPosts.component';
+import Contact from './components/contact.component';
+import BlogDashboard from './components/blogDashboard.component';
+
+import Home from './pages/home';
+import Blogs from './pages/blogPosts';
+import About from './pages/about';
+import Projects from './pages/projects';
 
 export default function App () {
   
@@ -27,6 +50,8 @@ export default function App () {
   const [avatar, setAvatar] = useState('');
   const [userInput, setUserInput] = useState('');
   const [error, setError] = useState(null);
+  const [secretToken, setToken] = useState('');
+  const [logged, setLogged] = useState(false);
   
 
   useEffect(() => {
@@ -38,8 +63,24 @@ export default function App () {
         console.log(data);
     });
     
+    axios.get("/cookies")
+      .then((data) => {
+        setCookieData(data.data)
+      })
+      .catch((err) => {
+        return console.log(err)
+       });
+    
     
   }, [])
+  
+  const setCookieData = ({ token }) => {
+    const thetoken = token || ''
+    setToken(thetoken)
+    if (thetoken != '') {
+      setLogged(true)
+    }
+  }
   
   const setGithubData = ({ name, login, followers, following, public_repos, avatar_url}) => {
     setName(name)
@@ -54,32 +95,131 @@ export default function App () {
   return (
     <UserProvider>
       <BrowserRouter>
-        <div className="nav">
-          <h2>Welcome to my app</h2>
-          <Link to="/register">Click for users</Link>
-          <Link to="/blogs">Click for blogs</Link>
-          <Link to="/login">Click to login</Link>
+        
+       <nav className="nav fixed w-full z-50">
+        <div className="nav-container">
+          <div className="relative flex items-center justify-between h-16">
+            <MobileMenuButton />
+            <div className="desktop-menu">
+              <div className="menu-logo-wrapper">
+               { colorTheme === "light" ?
+                  <DarkLogo />
+                  :
+                  <LightLogo />
+               }
+              </div>
+              <div className="desktop-menu-wrapper">
+                <div className="nav-link-wrapper">
+
+                  <NavLink to="/" exact className="nav-link" activeClassName="current">Welcome</NavLink>
+
+                  <NavLink to="/about" className="nav-link" activeClassName="current">About Me</NavLink>
+
+                  <NavLink to="/blog" className="nav-link" activeClassName="current">Blog</NavLink>
+
+                  { logged 
+                      ? <NavLink to="/blog-dashboard" className="nav-link" activeClassName="current">Dashboard</NavLink>
+                      : <NavLink to="/login" className="nav-link" activeClassName="current">Login</NavLink>
+                  }
+                  { logged 
+                      ? <NavLink to="/logout" className="nav-link" activeClassName="current">Logout</NavLink>
+                      : <></>
+                  }
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <Switch>
-          <Route path="/blogs">
-            <Blogs />
-          </Route>
-          <Route path="/register">
-            <RegisterUsers />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/contact">
-            <Login />
-          </Route>
-          <ProtectedRoute path="/blog-dashboard">
-            <Blogs />
-          </ProtectedRoute>
-          <Route exact={true} path="/">
-            <Home />
-          </Route>
-        </Switch>
+       <MobileMenu />
+       </nav>
+    
+        <div id="main" className="pt-16 absolute w-full h-full">
+
+          <div className="bg-indigo-100 z-2 shadow-lg dark:bg-gray-700 relative h-full min-h-screen md:w-50 lg:w-70 fixed">
+              <div className="xl:py-2 flex flex-col justify-start">
+                 { colorTheme === "light" ?
+                    <GithubPicture />
+                    :
+                    <California />
+                 }
+                  <div className="hidden xl:block text-grey-darker px-4 pb-3">
+                    <div className="stats flex rounded bg-primarylight dark:bg-gray-800 mb-4 items-center justify-between w-full px-2">
+                      <span className="relative flex dark:text-white" title="Followers">
+                        <UserSVG className="h-4 z-2 relative dark:text-white" />
+                        <span className="absolute top-0 right-0 inline-flex items-center justify-center px-1 py-1 mr-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-indigo-600 rounded-full text-xs z-1">{followers}</span>
+                      </span>
+                      <span className="relative flex dark:text-white" title="Public Repositories">
+                        <CodeSVG className="h-4 z-2 relative dark:text-white" />
+                        <span className="absolute top-0 right-0 inline-flex items-center justify-center px-1 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-indigo-600 rounded-full text-xs z-1">{repos}</span>
+                      </span>
+                      <span className="relative flex dark:text-white" title="Following">
+                        <UserCheckSVG className="h-4 z-2 relative" />
+                        <span className="absolute top-0 right-0 inline-flex items-center justify-center px-1 py-1 text-xs font-bold leading-none text-red-100 transform translate-x-1/2 -translate-y-1/2 bg-indigo-600 rounded-full text-xs z-1">{following}</span>
+                      </span>
+                    </div>
+                    <h1 className="font-mono uppercase font-bold text-lg">
+                      {name}
+                    </h1>
+                    <p className="text-xs py-2">
+                      I am a full stack software engineer currently working for <strong className="text-primarylight">Intrinio, Inc.</strong> As a Software Engineer, I am responsible for implementing visual elements for our web products and applications. I combine the art of design with the science of programming providing the hemispheric synchronization bridge between the two departments.I am responsible for the translation of UI/UX design wireframes to production-ready code.
+                    </p>
+                  </div>
+                  <div className="hidden xl:block uppercase font-bold text-grey-darker text-xs px-4 py-2">
+                    Main
+                  </div>
+                  <div className="group relative sidebar-item with-children">
+                    <NavLink to="/projects" className="block xl:flex xl:items-center text-center xl:text-left shadow-light xl:shadow-none py-6 xl:py-2 xl:px-4 border-l-4 border-transparent hover:bg-primarylight dark:hover:bg-indigo-400" activeClassName="bg-primarylight dark:bg-indigo-400">
+                      <DashboardSVG />
+                      <div className="text-black font-bold dark:text-white text-xs">View Github Projects</div>
+                    </NavLink>
+                  </div>
+                  <div className="group relative sidebar-item with-children">
+                    <NavLink to="/contact" className="block xl:flex xl:items-center text-center xl:text-left shadow-light xl:shadow-none py-6 xl:py-2 xl:px-4 border-l-4 border-transparent hover:bg-primarylight dark:hover:bg-black" activeClassName="bg-primarylight dark:bg-indigo-400">
+                      <CommentSVG />
+                      <div className="text-black font-bold dark:text-white text-xs">Contact Me</div>
+                    </NavLink>
+                  </div>
+                </div>
+
+
+                <div className="py-4">
+                  <div className="hidden xl:block uppercase font-bold text-grey-darker text-xs px-4 py-2">
+                    Controls
+                  </div>
+
+                  <div className="flex items-center justify-start px-4 mt-3">
+                    <label htmlFor="checkbox" className="flex items-center cursor-pointer text-xs">
+                      <div className="relative mr-3">
+                        <input
+                          type="checkbox"
+                          id="checkbox"
+                          name="checkbox"
+                          className="sr-only"
+                          checked={colorTheme === "light" ? true : false}
+                          onChange={() => setTheme(colorTheme)} />
+                        <div className="block bg-gray-600 w-8 h-5 rounded-full"></div>
+                        <div className="dot absolute left-1 top-1 bg-white w-3 h-3 rounded-full transition"></div>
+                      </div>
+                      {colorTheme === "light" ? "Dark" : "Light"} Mode
+                    </label>
+                  </div>
+                </div>
+              </div>
+          <div className="bg-indigo-200 z-0 dark:bg-gray-600 pt-8 overflow-auto pb-24" style={{maxHeight: '92.5vh'}}>
+
+            <Switch>
+              <Route path="/about" component={About} />
+              <Route path="/projects" component={Projects} />
+              <Route path="/blog" component={Blogs} />
+              <Route path="/register" component={RegisterUsers} />
+              <Route path="/login" component={Login} />
+              <Route path="/contact" component={Contact} />
+              <ProtectedRoute path="/blog-dashboard" component={BlogDashboard} loggedIn={logged} />
+              <Route exact={true} path="/" component={Home} />
+            </Switch>
+          </div>
+        </div>
+
       </BrowserRouter>
     </UserProvider>
   );
