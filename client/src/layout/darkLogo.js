e5278d60748020ae535125c043df5acedfd10da9
Nicola Reyes <nreyes@intrinio.com>
2021-05-13 16:21:27 -0600

    if dark mode text should be light

--- client/src/layout/darkLogo.js
+++ client/src/layout/darkLogo.js
@@ -2,7 +2,7 @@ export default function DarkLogo(){
   return(
       <div className="mr-5 flex items-center">
         <img className="menu-logo block mr-4" src="dark-mode-mobile.png" alt="Workflow" />
-        <div className="font-mono text-xl font-bold">NicolaScribbles</div>
+        <div className="font-mono text-xl font-bold text-merlin-100">NicolaScribbles</div>
       </div>
     )
 }
