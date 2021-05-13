dede79db3724004e7ea6a768298b49be0910f3f9
Nicola Reyes <nreyes@intrinio.com>
2021-05-13 16:21:49 -0600

    updating about me page

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -1,29 +1,33 @@
 import React from 'react';
 export default function About(){
   return(
-    <div className="container max-w-6xl mx-8 h-v-100">
-      <div className="x-20 max-w-100 lg:mx-auto px-5 py-10 rounded-xl text-center bg-indigo-50 dark:bg-gray-700 dark:shadow-xl lg:flex justify-center overflow-y-auto">
+    <div className="container max-w-full md:max-w-4xl lg:max-w-5xl xl:max-w-6xl mx-8 h-v-100">
+      
+      <div className="x-20 max-w-100 lg:mx-auto px-5 py-10 rounded-xl text-center bg-gray dark:bg-merlin dark:text-pampas shadow-xl lg:flex justify-center overflow-y-auto">
+        <iframe src="https://resume.io/r/KsCU2bcPG" title="My Resume" width="100%" />
+      </div>
+      <div className="x-20 max-w-100 lg:mx-auto px-5 py-10 rounded-xl text-center bg-gray dark:bg-merlin dark:text-pampas shadow-xl lg:flex justify-center overflow-y-auto">
         <div className="overflow-hidden">
           <div className="border-gray-200 border-2 rounded-lg">
             <dl>
               <div className="bg-gray-50 dark:bg-gray-700 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 rounded-lg">
-                <dt className="text-sm font-medium text-gray-500 dark:text-white">
+                <dt className="text-sm font-medium text-gray-900 dark:text-white">
                   Stack
                 </dt>
                 <dd className="mt-1 text-sm text-gray-900  dark:text-gray-200 sm:mt-0 sm:col-span-2">
                   HTML, HAML, CSS, SASS, SCSS, Less, Stylus, TailwindCSS, Javascript, Typescript, Coffeescript, Node.js, NPM, CLI, Docker, AngularJS, VueJS, ReactJS, PostgresQL, Ruby, Padrino for Ruby.
                 </dd>
               </div>
-              <div className="px-4 py-5 dark:bg-gray-900 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500 dark:text-white">
+              <div className="px-4 py-5 bg-cashmere-300 dark:bg-gray-900 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-900 dark:text-white">
                   Specialty
                 </dt>
                 <dd className="mt-1 text-sm text-gray-900 dark:text-white sm:mt-0 sm:col-span-2">
                   Front End Development. UI/UX principles. Integrations.
                 </dd>
               </div>
-              <div className="bg-gray-50  dark:bg-gray-700 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                <dt className="text-sm font-medium text-gray-500 dark:text-white">
+              <div className="bg-gray-50 dark:bg-gray-700 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-900 dark:text-white">
                   Other Interests
                 </dt>
                 <dd className="mt-1 text-sm text-gray-900 dark:text-gray-200 sm:mt-0 sm:col-span-2">
@@ -31,7 +35,7 @@ export default function About(){
                 </dd>
               </div>
               <div className="px-4 py-5 dark:bg-gray-900 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 rounded-lg">
-                <dt className="text-sm font-medium text-gray-500 dark:text-white">
+                <dt className="text-sm font-medium text-gray-900 dark:text-white">
                   Attachments
                 </dt>
                 <dd className="mt-1 text-sm text-gray-900 dark:text-white sm:mt-0 sm:col-span-2">
@@ -73,7 +77,6 @@ export default function About(){
           </div>
         </div>
       </div>
-      
     </div>
   )
 }
