e75585a38156a944c42d273c0a3ec3c69db1b0a2
Nicola Reyes <nreyes@intrinio.com>
2021-05-13 00:18:26 -0600

    svgs

---
+++ client/src/svgs/user.js
@@ -0,0 +1,5 @@
+export default function UserSVG() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" className="h-6 w-6 fill-current xl:mr-2"><path d="M12 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm9 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v2z" className="heroicon-ui"></path></svg>
+  )
+}
