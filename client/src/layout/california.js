e557626d541e589e8baf976ba27f3d5ae0a5e781
Nicola Reyes <nreyes@intrinio.com>
2021-05-13 16:20:43 -0600

    hide in smaller layouts

--- client/src/layout/california.js
+++ client/src/layout/california.js
@@ -1,6 +1,6 @@
 export default function California(){
   return(
-  <figure className="px-4 text-xs mb-5">
+  <figure className="px-4 text-xs mb-5 hidden lg:block">
     <img src="square-portrait.jpg" className="w-52 h-52 mt-2 mx-auto rounded-lg shadow-dark mb-3 z-2" wrapped="true" alt="GithubProfile Avatar" />
     <figcaption>Taken at the Santa Monica Pier</figcaption>
   </figure>
